439867ad97fc7700b2e64c62a80938e766c2ae0d
rokkyw <rokkyw@naver.com>
2021-04-21 17:41:37 +0900

    Modifying app.js

--- server/app.js
+++ server/app.js
@@ -1,9 +1,7 @@
-
 const express = require("express");
-const os = require("os");
-
 const app = express();
 const PORT = 80;
+
 const time = new Date();
 const date = time.getFullYear().toString() + '/' + (time.getMonth() + 1).toString() + '/' + time.getDate().toString(); 
 const curTime = time.getHours().toString() + ":" + time.getMinutes().toString();
@@ -17,3 +15,4 @@ const sayHello = (req, res) => res.send(`This is Server Test~! Server Run at ${d
 
 app.get('/', sayHello);
 app.listen(PORT, handleListening); 
+
