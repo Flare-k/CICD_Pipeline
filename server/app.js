99f92bd59308753902b18bb4da9f862e7c8cd9fb
rokkyw <rokkyw@naver.com>
2021-04-21 13:41:40 +0900

    Modified app.js

--- server/app.js
+++ server/app.js
@@ -11,28 +11,12 @@ const PORT = 80;
 const time = new Date();
 const date = time.getFullYear().toString() + '/' + (time.getMonth() + 1).toString() + '/' + time.getDate().toString(); 
 const curTime = time.getHours().toString() + ":" + time.getMinutes().toString();
-const net = os.networkInterfaces();
-const res = {};
-
-for (const value of Object.keys(net)) {
-    for (const n of net[value]) {
-        if (n.family == 'IPv4' && !n.interval) {
-            if (!res[value]) {
-                res[value] = [];
-            }
-            res[value].push(n.address);
-        }
-    }
-}
-
-const ip = res['Loopback Pseudo-Interface 1'][0];
 
 function handleListening() {
-    console.log(`Listening on: http://${ip}:${PORT} - Server Run at ${date} ${curTime}`);
-
+    console.log(`Listening on: http://localhost:${PORT}`);
 }
 
-const sayHello = (req, res) => res.send("This is Server Test~!");
+const sayHello = (req, res) => res.send(`This is Server Test~! Server Run at ${date} ${curTime}`);
 
 /*
 app.use(cookieParser());
