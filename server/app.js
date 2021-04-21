12f368852a15082a6175c15c6b08e79e0e45186c
rokkyw <rokkyw@naver.com>
2021-04-21 13:25:15 +0900

    Current IP and TimeStamp

--- server/app.js
+++ server/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const os = require("os");
 /*
 import morgan from "morgan";
 import helmet from "helmet";
@@ -7,9 +8,28 @@ import cookieParser from "cookie-parser";
 */
 const app = express();
 const PORT = 80;
+const time = new Date();
+const date = time.getFullYear().toString() + '/' + (time.getMonth() + 1).toString() + '/' + time.getDate().toString(); 
+const curTime = time.getHours().toString() + ":" + time.getMinutes().toString();
+const net = os.networkInterfaces();
+const res = {};
+
+for (const value of Object.keys(net)) {
+    for (const n of net[value]) {
+        if (n.family == 'IPv4' && !n.interval) {
+            if (!res[value]) {
+                res[value] = [];
+            }
+            res[value].push(n.address);
+        }
+    }
+}
+
+const ip = res['Loopback Pseudo-Interface 1'][0];
 
 function handleListening() {
-    console.log(`Listening on: http://localhost:${PORT}`);
+    console.log(`Listening on: http://${ip}:${PORT} - Server Run at ${date} ${curTime}`);
+
 }
 
 const sayHello = (req, res) => res.send("This is Server Test~!");
